e4efeafee81161d55db8e48c85d4b4975f82942c
Anyaegbunam-Alexander <alexanderking.aa@gmail.com>
2023-03-12 15:49:30 +0100

    updated config

--- api/config/config.py
+++ api/config/config.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datetime import timedelta
 from decouple import config
 
@@ -6,6 +7,10 @@ from decouple import config
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 PARENT_DIR = os.path.abspath(os.path.join(BASE_DIR, os.pardir))
 GRANDPARENT_DIR = os.path.abspath(os.path.join(PARENT_DIR, os.pardir))
+
+uri = config('DATABASE_URL')  # or other relevant config var
+if uri.startswith('postgres://'):
+    uri = uri.replace('postgres://', 'postgresql://', 1)
 
 class Config:
     SECRET_KEY=config('SECRET_KEY', 'secret')
@@ -35,7 +40,7 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = config('DEBUG', cast=bool)
 
